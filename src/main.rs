14e8f4c3ba16296f50abcdd1512ea2f46053b306
Jalil S <jalil@example.com>
2024-01-18 16:58:46 +0900

    Added insert query into subscribe handler/updated tests, used pgpool

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::net::TcpListener;
 use newsletter::{configuration::get_configuration, startup::run};
+use sqlx::PgPool;
 use sqlx::{Connection, PgConnection};
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, db_pool)?.await
 }
