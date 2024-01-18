459a044dcc4233042a7d7317b839a70d343eedf5
Jalil S <jalil@example.com>
2024-01-18 15:19:43 +0900

    Added app-state to subscribe handler with weith webData

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::net::TcpListener;
-
 use newsletter::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
